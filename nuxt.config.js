728c4d0d1aabca9f287103d7236d16382bf759a9
Lawrence <lawbraun.almeida@gmail.com>
2019-04-04 16:53:23 +0100

    add Vuex Actions to get Users/Comments & setup .env;

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,8 @@ module.exports = {
   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv'
   ],
   /*
   ** Axios module configuration
---
+++ store/actions.js
@@ -0,0 +1,8 @@
+export default {
+  async GET_USERS({ }) {
+    return await this.$axios.$get(`${ process.env.USERS_ENDPOINT }`)
+  },
+  async GET_COMMENTS({ }) {
+    return await this.$axios.$get(`${ process.env.COMMENTS_ENDPOINT }`)
+  },
+}
