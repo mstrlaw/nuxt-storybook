85b30d534edeadd7870d0733aa4a51632df38342
Lawrence <lawbraun.almeida@gmail.com>
2019-04-04 16:35:52 +0100

    Enhancing the List component & adding SASS pre-processors

--- components/list/List.vue
+++ components/list/List.vue
@@ -1,17 +1,52 @@
 <template>
-  <div class="list">
-    I'm a list
+  <div class="list p-5 rounded">
+    I'm a {{ source }} list
   </div>
 </template>
 
 <script>
   export default {
-    name: 'List'
+    name: 'List',
+    props: {
+      source: {
+        type: String,
+        default: 'users'
+      }
+    },
+    data() {
+      return {
+        entities: []
+      }
+    },
+    mounted() {
+      switch (this.source) {
+        default:
+        case 'users':
+          this.loadUsers()
+          break
+
+        case 'comments':
+          this.loadUsers()
+          break
+      }
+    },
+    methods: {
+      loadUsers() {
+        //  Will call store action
+        console.log('load users')
+      },
+      loadComments() {
+        //  Will call store action
+        console.log('load comments')
+      },
+    }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  $background: #EFF8FF;
+
   .list {
-    background: #CCC;
+    background: $background;
   }
 </style>
